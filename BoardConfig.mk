1f6795dbc34c5da6287308c06238eb601870cfe8
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-25 15:29:52 -0600

    hltechn: Use new full defconfig from new kernel
    Change-Id: I4db49e423d556cb67b9d82925907a3b30fc466fa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ TARGET_INIT_VENDOR_LIB := libinit_msm8974
 TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/hlte/init/init_hlte.cpp
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_chn_defconfig
+TARGET_KERNEL_CONFIG := lineage_hltechn_defconfig
 
 # NFC
 BOARD_NFC_HAL_SUFFIX := $(TARGET_BOARD_PLATFORM)
